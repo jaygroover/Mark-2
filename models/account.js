b81ef842d6e994cee4499e50b973cf04d099da1e
jaygroover1 <jaygroover@gmail.com>
2019-07-15 21:02:53 -0400

    working server

---
+++ models/account.js
@@ -0,0 +1,53 @@
+// // model name is Account
+// // next define all the properties in Account
+// module.exports = function(sequelize, DataTypes) {
+//   var Account = sequelize.define("account", {
+//     organization: {
+//       type: DataTypes.STRING,
+//       isNull: true
+//     },
+
+//     userName: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     address: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     city: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     state: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     zipcode: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     contactName: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     phoneNumber: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     faxNumber: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       notNull: true
+//     }
+//   });
+
+//   return Account;
+// };
---
+++ models/index.js
@@ -0,0 +1,48 @@
+// "use strict";
+
+// var fs = require("fs");
+// var path = require("path");
+// var Sequelize = require("sequelize");
+// var basename = path.basename(module.filename);
+// var env = process.env.NODE_ENV || "development";
+// var config = require(__dirname + "/../config/config.json")[env];
+
+// // empty object that gets attached to the models
+// var db = {};
+
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable],config);
+// } else {
+//   var sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
+
+// fs.readdirSync(__dirname)
+//   .filter(function(file) {
+//     return (
+//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+//     );
+//   })
+//   .forEach(function(file) {
+//     var model = sequelize.import(path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(function(modelName) {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// // attaches the sequelize package and Sequelize constructor to our empty db object
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// // allows us to get all the files in models and add them to the db object
+// // the purpose of this file gives us access to ALL our models in one object that we can then call in any file
+// // remember in the server.js file to set db to read the index.js file
+// module.exports = db;
